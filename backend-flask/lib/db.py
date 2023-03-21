715d0b4cdb6d2216797a7777f4d155386c401347
seanware <seanaware@gmail.com>
2023-03-21 21:20:34 +0000

    Finished Utility scripts

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -35,14 +35,14 @@ class Db:
     for key, value in params.items():
       print(key, ":", value)
 
-  def print_sql(self,title,sql):
+  def print_sql(self,title,sql, params={}):
     cyan = '\033[96m'
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
-    print(sql)
+    print(sql , params)
     
   def query_commit(self,sql,params={}):
-    self.print_sql('commit with returning',sql)
+    self.print_sql('commit with returning',sql,params)
 
     pattern = r"\bRETURNING\b"
     is_returning_id = re.search(pattern, sql)
@@ -60,7 +60,7 @@ class Db:
       self.print_sql_err(err)
   # when we want to return a json object
   def query_array_json(self,sql,params={}):
-    self.print_sql('array',sql)
+    self.print_sql('array',sql,params)
 
     wrapped_sql = self.query_wrap_array(sql)
     with self.pool.connection() as conn:
@@ -71,7 +71,7 @@ class Db:
   # When we want to return an array of json objects
   def query_object_json(self,sql,params={}):
 
-    self.print_sql('json',sql)
+    self.print_sql('json',sql, params)
     self.print_params(params)
     wrapped_sql = self.query_wrap_object(sql)
 
@@ -83,6 +83,16 @@ class Db:
           "{}"
         else:
           return json[0]
+
+   # when we want to return a a single value
+  def query_value(self,sql,params={}):
+    self.print_sql('value',sql,params)
+
+    with self.pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(sql,params)
+        json = cur.fetchone()
+        return json[0]
 
   def query_wrap_object(self,template):
     sql = f"""
@@ -100,7 +110,7 @@ class Db:
     """
     return sql
 
-  def print_sql_err(self,err):
+  def print_sql_err(self,err, params):
     # get details about the exception
     err_type, err_obj, traceback = sys.exc_info()
 
