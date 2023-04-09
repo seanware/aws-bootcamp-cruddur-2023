fc624677a8955a89157b75f73659c47ca5cfa98b
seanware <seanaware@gmail.com>
2023-04-09 16:35:50 +0000

    Create bucket with Enviroment variables

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -7,22 +7,12 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucketName: string = process.env.THUMBING_BUCKET_NAME as string;
 
-    const bucketName: string  = process.env.THUMBING_BUCKET_NAME as string;
-    const bucket = this.createBucket(bucketName);    
-
-    
-  }
-
-  createBucket(bucketName: string): s3.IBucket {
     const bucket = new s3.Bucket(this, 'ThumbingBucket', {
       bucketName: bucketName,
-      removalPolicy: cdk.RemovalPolicy.DESTROY
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
-    return bucket;
+  
   }
-
-  
-  
 }
